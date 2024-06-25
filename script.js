bc9db3fc26b914947f753f0d8cbbfccc239bfef7
YosephSE <yoseph.kedir10@gmail.com>
2024-06-26 00:52:55 +0300

    api data destructured

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
-
 function randomUserData() {
     const apiURL = 'https://randomuser.me/api/';
 
     fetch(apiURL)
         .then(response => response.json())
         .then(data => {
-            document.write(JSON.stringify(data))
-            const userData = data.results[0];
+            let user = (data.results[0])
+            // document.write(JSON.stringify(user))
+            let fullName = `${user.name.title} ${user.name.first} ${user.name.last}`
+            let userName = user.login.username
+            console.log(fullName)
+            console.log(userName)
+            let age = user.dob.age
+            console.log(age)
+            let proflie = user.picture.large
+            console.log(proflie)
+            
         })
         .catch(error => {
             console.error('Error fetching data:', error);
