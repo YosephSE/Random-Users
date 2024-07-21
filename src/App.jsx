561db1ba928d88c651bfe33adc8c416b0ad30e23
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-21 22:03:04 +0300

    useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [users, setUsers] = useState([])
+useEffect (() => {
+  axios.get('https://randomuser.me/api/').then((res) =>{
+    setUsers((users) => users.push(res.data.results[0]))
+  }, users)
+})
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">React Random Users From API</div>
+      <div className="grid-container">
+      <div className="grid-item">{users}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
