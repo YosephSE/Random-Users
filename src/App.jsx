2e9b52845fbc43069c52fa5c64eb6a46300e98d1
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-07-21 22:25:15 +0300

    api working ' '

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [users, setUsers] = useState([])
-useEffect (() => {
-  axios.get('https://randomuser.me/api/').then((res) =>{
-    setUsers((users) => users.push(res.data.results[0]))
-  }, users)
-})
+  const [users, setUsers] = useState([]);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      if (count < 99) {
+        try {
+          const res = await axios.get("https://randomuser.me/api/");
+          setUsers((prevUsers) => [...prevUsers, res.data.results[0]]);
+          setCount((prevCount) => prevCount + 1);
+        } catch (error) {
+          console.error("Error fetching user data:", error);
+        }
+      }
+    };
+
+    fetchUsers();
+  }, [count]);
+
   return (
     <>
       <div className="header">React Random Users From API</div>
       <div className="grid-container">
-      <div className="grid-item">{users}</div>
+        {users.map((user, index) => (
+          <div key={index} className="grid-item">
+            <img
+              src={user.picture.large}
+              alt={`${user.name.first}'s Profile pic`}
+            />
+            <p>
+              {user.name.first} {user.name.last}
+            </p>
+            <p>{user.gender}</p>
+            <p>{user.email}</p>
+          </div>
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
